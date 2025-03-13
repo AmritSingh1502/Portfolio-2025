e35eff25469df95bfe5f06228ab01a8b9f89026c
AmritSingh1502 <113255930+AmritSingh1502@users.noreply.github.com>
2025-03-13 12:53:15 +0530

    initial commit

---
+++ web/components/Navbar.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import Link from 'next/link'
+ import React, {  useState } from 'react'
+ import { AiOutLineMenu, AiOutlineClose } from 'react-icons/ai'
+
+ const navLinks = [
+    { title: 'About', path: '#about' },
+    { title: 'Portfolio', path: '#portfolio' },
+    { title: 'Contact', path: '#contact' },
+    { title: 'stack', path: '#stack' },
+ ]
+export const Navbar = () => {
+    const [nav, setNav] = useState(false)
+
+    const toggleNav = () => {
+        setNav(!nav)
+    }
+
+    const closeNav = () => {
+        setNav(false)
+    }
+
+
+  return (
+    <div className="z-50 fixed flex justify-center w-full text-white font-bold">
+
+        <div className="border border-white/20 mt-8 backdrop-blur-3xl rounded-3xl hidden md:flex items-center justify-center p-2 max-w-[400px] mx-auto">
+        <ul className='flex flex-row p-2 space-x-8'>
+            {navLinks.map((link, index) => (
+                <li key={index}>
+                    <Link href={link.path} className="transform hover:text-white/50 transition-all duration-300 ease-in-out">
+                        {link.title}
+                    </Link>
+                </li>
+            ))}
+
+        </ul>
+        </div>
+
+        <div onClick={toggleNav} className="md:hidden absolute  top-5 right-14 border rounded z-50 text-white/70 border-white/70 p-2">
+        {nav ? <AiOutlineClose size={30} /> : <AiOutLineMenu size={30} />}
+        </div>
+    </div>
+  )
+}
